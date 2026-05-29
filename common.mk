146d0fa1d9d70c2bc5266dccb859d6ad35920ced
TheSillyOk <194730369+TheSillyOk@users.noreply.github.com>
2026-05-28 21:36:20 -0300

    update: bump version
    Signed-off-by: TheSillyOk <194730369+TheSillyOk@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
 ROOT_DIR ?= .
 
-VER_NAME ?= v1
+VER_NAME ?= v1.0.1
 VER_CODE ?= $(shell git -C "$(ROOT_DIR)" rev-list HEAD --count 2>/dev/null || echo 1)
 COMMIT_HASH ?= $(shell git -C "$(ROOT_DIR)" rev-parse --verify --short HEAD 2>/dev/null || echo unknown)
 
